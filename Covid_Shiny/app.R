542a08ed11614fd0a9dd97b6d459ee403acda6a1
p.griebel <p.griebel@ikmb.uni-kiel.de>
2021-02-01 19:25:58 +0100

    Added. navbar and second viz for paramter countries.

--- Covid_Shiny/app.R
+++ Covid_Shiny/app.R
@@ -26,34 +26,61 @@ our_world_in_data <- read_csv(
 
 
 # Define UI for application
-ui <- fluidPage(theme = shinytheme("flatly"),
-
-    # Application title
-    titlePanel("Covid19 Data"),
-
-    # Sidebar with a slider input for dates
-    sidebarLayout(
-        sidebarPanel(
-            width = 2,
-            selectInput(inputId = 'mycontinent', label = "continent", choices = c("Europe", "Asia")),
-            sliderInput(inputId = 'mydate', label = "date", 
-                        min = as.Date("2020-01-22", "%Y-%m-%d"),
-                        max = Sys.Date()-1,
-                        value = Sys.Date()-1),
-        ),
-        
-    mainPanel(
-        width = 10,
+ui <- navbarPage(theme = shinytheme("flatly"), title = "Covid19-Data",
+                 
+    tabPanel("Cases",
+        # Plots
         tabsetPanel(
-            tabPanel("Cases", plotOutput('total_cases')), 
+            tabPanel("Total Cases", plotOutput('total_cases')), 
             tabPanel("Cases/100k", plotOutput('Cases_100k'))
-        )
-    )
-))
+            ),
+    
+        # separating line   
+        hr(),
 
-# Define server logic required to draw a histogram
+        # Continent-input & slider-bar for dates
+        fluidRow(
+            column(3,
+                selectInput(inputId = 'mycontinent', label = "Continent", choices = unique(our_world_in_data$continent))
+                ),
+                
+            column(width = 8, offset = 1,
+                sliderInput(inputId = 'mydate', label = "Date", 
+                            min = as.Date("2020-01-22", "%Y-%m-%d"),
+                            max = Sys.Date()-1,
+                            value = Sys.Date()-1)
+                )
+        )),
+
+    tabPanel("Deaths",
+         # Plots
+         tabsetPanel(
+             tabPanel("Total Deaths", plotOutput('total_deaths')), 
+             tabPanel("Deaths/100k", plotOutput('deaths_100k'))
+         ),
+         
+         # separating line   
+         hr(),
+         
+         # Continent-input & slider-bar for dates
+         fluidRow(
+             column(3,
+                    selectInput(inputId = 'mycontinent2', label = "Continent", choices = unique(our_world_in_data$continent))
+             ),
+             
+             column(width = 8, offset = 1,
+                    sliderInput(inputId = 'mydate2', label = "Date", 
+                                min = as.Date("2020-01-22", "%Y-%m-%d"),
+                                max = Sys.Date()-1,
+                                value = Sys.Date()-1)
+             )
+         )))
+
+
+# Define server logic 
 server <- function(input, output) {
-
+    
+    # plots for 'Cases' tab
     output$total_cases <- renderPlot({
         our_world_in_data %>% 
         select(c(date, continent, location, total_cases)) %>% 
@@ -80,6 +107,36 @@ server <- function(input, output) {
             geom_bar(stat = "identity")+
             scale_x_continuous(labels = function(x) format(x, scientific = FALSE)) +
             labs(x = "Total Cases / 100.000", y = " ") +
+            theme(legend.position = "none") 
+    })
+    
+    # plots for 'deaths' tab
+    output$total_deaths <- renderPlot({
+        our_world_in_data %>% 
+            select(c(date, continent, location, total_deaths)) %>% 
+            mutate_if(is.numeric, ~replace(., is.na(.), 0)) %>%
+            filter(continent == input$mycontinent2) %>%
+            filter(date == input$mydate2) %>% 
+            ggplot(aes(total_deaths, reorder(location, total_deaths),
+                       fill = ifelse(total_deaths == max(total_deaths), "highlighted", "normal")))+
+            geom_bar(stat = "identity")+
+            scale_x_continuous(labels = function(x) format(x, scientific = FALSE)) +
+            labs(x = "Total Deaths", y = " ") +
+            theme(legend.position = "none") 
+    })
+    
+    output$deaths_100k <- renderPlot({
+        our_world_in_data %>% 
+            select(c(date, continent, location, total_deaths_per_million)) %>% 
+            mutate_if(is.numeric, ~replace(., is.na(.), 0)) %>%
+            mutate(total_deaths_per_100k = total_deaths_per_million/10) %>% 
+            filter(continent == input$mycontinent2) %>%
+            filter(date == input$mydate2) %>% 
+            ggplot(aes(total_deaths_per_100k, reorder(location, total_deaths_per_100k),
+                       fill = ifelse(total_deaths_per_100k == max(total_deaths_per_100k), "highlighted", "normal")))+
+            geom_bar(stat = "identity")+
+            scale_x_continuous(labels = function(x) format(x, scientific = FALSE)) +
+            labs(x = "Total Deaths / 100.000", y = " ") +
             theme(legend.position = "none") 
     })
    
